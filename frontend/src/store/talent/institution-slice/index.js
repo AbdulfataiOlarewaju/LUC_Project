4d73a661282715ce8bf0ba352524124723725ebb
Abdulfatai Olarewaju <olarewaju Abdulfatai>
2026-05-02 15:59:08 +0100

    api issue solved

--- frontend/src/store/talent/institution-slice/index.js
+++ frontend/src/store/talent/institution-slice/index.js
@@ -8,12 +8,21 @@ const initialState = {
   error: null,
 };
 
-// fetch All institutions
+// fetch All institutionsyy
 export const fetchInstitutions = createAsyncThunk(
   "institutions/fetchInstitutions",
   async (_, { rejectWithValue }) => {
     try {
-const result = await axios.get("/api/institutions");
+<<<<<<< HEAD
+      // Use vite proxy on development, full URL on production
+      const apiUrl = import.meta.env.PROD 
+        ? "https://luc-m8t9.onrender.com/api/institutions"
+        : "/api/institutions";
+      
+=======
+      const apiUrl = getApiUrl("/api/institutions");
+>>>>>>> Abdulfatai/frontend
+      const result = await axios.get(apiUrl);
       return result.data;
       console.log(result.data);
       
